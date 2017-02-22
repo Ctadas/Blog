03ef692f37f6c1182cd425ed16a84c396610e110
dada <1226184067@qq.com>
2017-02-22 13:31:26 +0800

    修改base视图

--- blogs/views.py
+++ blogs/views.py
@@ -1,57 +1,53 @@
 from django.shortcuts import render
 from blogs.models import Category,Page 
 
-# Create your views here.
-def index(request):
-	context_dict = {}
-	category_list = Category.objects.all()
+# 主页
+def index(request,context_dict):
 	page_list = Page.objects.order_by('-time')[:3]
-	page_view = Page.objects.order_by('-views')[:5]
-	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
-	context_dict['pages_view'] = page_view
 
 	return render(request,'blog/index.html',context_dict)
 
-def category(request,category_name):
-	context_dict = {}
+# 分类页
+def category(request,name_list,context_dict):
 	try:
-		categories = Category.objects.get(name = category_name)
+		categories = Category.objects.get(name = name_list)
 		pages = Page.objects.filter(category=categories)
 		context_dict['category'] = categories
 		context_dict['pages'] = pages
-
-		category_list = Category.objects.all()
-		context_dict['categories'] = category_list
-		page_view = Page.objects.order_by('-views')[:5]
-		context_dict['pages_view'] = page_view
-
 	except Category.DoesNotExist:
 		pass 
+
 	return render(request,'blog/category.html',context_dict) 
 
-def pages(request,page_title):
-	context_dict = {}
+# 文章页
+def pages(request,name_list,context_dict):
 	try:
-		page = Page.objects.get(title = page_title)
+		page = Page.objects.get(title = name_list)
 		context_dict['page'] = page
-
-		category_list = Category.objects.all()
-		context_dict['categories'] = category_list
-		page_view = Page.objects.order_by('-views')[:5]
-		context_dict['pages_view'] = page_view
-
 	except Page.DoesNotExist:
 		pass
+
 	return render(request,'blog/page.html',context_dict)
 
-def blog(request):
-	context_dict = {}
+# 所有文章页
+def blog(request,context_dict):
 	page = Page.objects.order_by('-time')
 	context_dict['pages']=page
 
+	return render(request,'blog/blog.html',context_dict)
+
+# 处理base模板显示
+def a (request,func,name_list):
+	context_dict = {}
 	category_list = Category.objects.all()
 	context_dict['categories'] = category_list
 	page_view = Page.objects.order_by('-views')[:5]
 	context_dict['pages_view'] = page_view
-	return render(request,'blog/blog.html',context_dict)
+	func_name = eval(func)  #用来计算在字符串中的有效Python表达式,并返回一个对象
+	
+	if func =='blog' or func =='index':
+		return func_name(request,context_dict)
+
+	else:
+		return func_name(request,name_list,context_dict)
